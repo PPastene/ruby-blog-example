5ae587952b00077ebc1d4924e38d4e4bb619551a
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-09-26 00:32:20 +0000

    Test for articles and comments

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article with no or short title" do
+    article = Article.new
+    assert_not article.save
+    article.title = '123'
+    assert_not article.save
+  end
+
+  test "save article" do
+    article = Article.new(title: '123456')
+    assert article.save
+  end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save comment without commenter or comment" do
+    article = Article.new(title: '123456')
+    article.save
+    comment = Comment.new(article_id: article.id)
+    assert_not comment.save
+    comment.commenter = "PHP Lover"
+    assert_not comment.save
+    comment.commenter = nil
+    comment.body = "PHP rulez!"
+    assert_not comment.save
+  end
+
+  test "save comment to article" do
+    article = Article.new(title: '123456')
+    article.save
+    comment = Comment.new(commenter: "A beloved Ruby User", body: "Ruby rules!!!!", article_id: article.id)
+    assert comment.save
+  end
 end
